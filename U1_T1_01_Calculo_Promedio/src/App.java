cb752931d6ea23d6311339c4b980d9ccc429d1d9
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-05-27 21:31:29 -0600

    Agregando métodos aprobado y calcularPromedio

--- U1_T1_01_Calculo_Promedio/src/App.java
+++ U1_T1_01_Calculo_Promedio/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-    }
-}
+        double promedio;
+        Estudiante estudiante1 = new Estudiante();
+
+        estudiante1.setNota(0, 9.8);
+        estudiante1.setNota(1, 7.4);
+        estudiante1.setNota(2, 10);
+        estudiante1.setNota(3, 9.8);
+        estudiante1.setNota(4, 9.8);
+        promedio = estudiante1.calularPromedio();
+        System.out.println(promedio);
+}}
+
--- U1_T1_01_Calculo_Promedio/src/Estudiante.java
+++ U1_T1_01_Calculo_Promedio/src/Estudiante.java
@@ -4,7 +4,10 @@ public class Estudiante {
     //Atributos declarados
     private String nombre, matricula;
     private double[] notas = new double[5];
+    double suma;
     
+
+    //Getters and Setters
     public String getNombre() {
         return nombre;
     }
@@ -21,7 +24,22 @@ public class Estudiante {
         return notas[i];
     }
     public void setNota(int i, double n) {
-        this.notas = notas;
+        notas[i] = n;
+    }
+
+    //Método para obtener el promedio
+    public double calularPromedio(){
+        for(int j = 0; j <5; j++){
+            suma = suma + notas[j];
+        }
+
+        return this.suma= suma/5;
+    }
+
+    //Método para saber si el promedio es mayora 70, y guardar si es true or false en una variable
+    public boolean aprobado(){
+        boolean aprobado = suma >= 70;
+        return aprobado;
     }
 
     
