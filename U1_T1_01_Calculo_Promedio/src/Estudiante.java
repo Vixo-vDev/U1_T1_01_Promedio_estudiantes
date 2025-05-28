436713fe6211223b6342d7b20ef9694104e2af2a
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-05-27 21:06:39 -0600

    Agregando getters and setters

--- U1_T1_01_Calculo_Promedio/src/Estudiante.java
+++ U1_T1_01_Calculo_Promedio/src/Estudiante.java
@@ -4,6 +4,26 @@ public class Estudiante {
     //Atributos declarados
     private String nombre, matricula;
     private double[] notas = new double[5];
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public double getNota(int i) {
+        return notas[i];
+    }
+    public void setNota(int i, double n) {
+        this.notas = notas;
+    }
 
+    
 
 }
